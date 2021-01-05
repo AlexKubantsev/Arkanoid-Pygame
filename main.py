867e810e3ffb0a27675fa3368eb4b22b776658de
AlexKubantsev <75995007+AlexKubantsev@users.noreply.github.com>
2021-01-05 15:01:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from random import randint
 
 
 class Ball(pygame.sprite.Sprite):
+    RADIUS = 10
+    SPEED = 5
+
     def __init__(self, game_window):
         super().__init__(game_window.all_sprites)
 
-        self.radius = 10
+        self.radius = Ball.RADIUS
         self.rect = pygame.Rect(0, 0, self.radius * 2, self.radius * 2)
-        self.speed = 5
+        self.speed = Ball.SPEED
 
     def draw(self, surf):
         pygame.draw.ellipse(surf, (255, 0, 0), self.rect)
@@ -48,11 +51,15 @@ class Block(pygame.sprite.Sprite):
 
 
 class Paddle(pygame.sprite.Sprite):
+    WIDTH = 90
+    HEIGHT = 7
+    SPEED = 10
+
     def __init__(self, game_window):
         super().__init__(game_window.all_sprites)
-        self.width, self.height = 90, 7
+        self.width, self.height = Paddle.WIDTH, Paddle.HEIGHT
         self.rect = pygame.Rect(0, 0, self.width, self.height)
-        self.speed = 10
+        self.speed = Paddle.SPEED
         self.win_width, self.win_height = game_window.get_window_size()
 
     def move_paddle(self, direction):
@@ -78,16 +85,26 @@ class Paddle(pygame.sprite.Sprite):
         return self.height
 
 
+class EndGameWindow:
+    def __init__(self, game_window):
+        game_window.screen.fill((0, 0, 0))
+        game_window.running = False
+
+
 class GameWindow:
+    FPS = 60
+    N_BLOCKS = 5
+    M_BLOCKS = 11
+
     def __init__(self, width, height, screen):
         self.width = width
         self.height = height
         self.opened_menu = None
         self.screen = screen
-        self.all_sprites = pygame.sprite.Group()
 
     def start_game(self):
         self.opened_menu.clear()
+        self.all_sprites = pygame.sprite.Group()
         self.running = True
         self.clock = pygame.time.Clock()
         self.paddle = Paddle(self)
@@ -99,23 +116,33 @@ class GameWindow:
 
         self.ball_x_direction = self.ball_y_direction = 1
         # block = Block(self)
-        FPS = 60
+        self.blocks_placement()
         while self.running:
             self.event_handler()
-            self.blocks_placement()
             if self.is_key_downed:
                 self.paddle.move_paddle(self.paddle_direction)
             self.screen.fill((0, 0, 0))
             self.ball.update(self.ball_x_direction, self.ball_y_direction)
             self.paddle.draw(self.screen)
             self.ball.draw(self.screen)
+            self.win_lost_detector()
+            block_hit_index = self.ball.rect.collidelist(list(map(lambda x: x.rect, self.blocks)))
+            self.blocks_collision_handler(block_hit_index)
+            self.blocks_draw()
             self.collision_handler()
+
             pygame.display.update()
             pygame.display.flip()
-            self.clock.tick(FPS)
+            self.clock.tick(GameWindow.FPS)
 
     def get_window_size(self):
         return self.width, self.height
+
+    def win_lost_detector(self):
+        if not self.blocks:
+            EndGameWindow(self)
+        if self.ball.rect.y > self.paddle.rect.y + self.paddle.rect.h:
+            EndGameWindow(self)
 
     def collision_handler(self):
         if self.ball.rect.colliderect(self.paddle.rect) and self.ball_y_direction > 0:
@@ -132,9 +159,18 @@ class GameWindow:
             self.ball_y_direction = -self.ball_y_direction
 
     def blocks_placement(self):
+        self.blocks = [
+            Block(self, i * (Block.WIDTH + Block.INDENT), j * (Block.HEIGHT + Block.INDENT)) for j in
+            range(GameWindow.N_BLOCKS) for i in range(GameWindow.M_BLOCKS)]
 
-        [Block(self, i * Block.WIDTH + Block.INDENT, j * Block.HEIGHT + Block.INDENT).draw(self.screen) for i in
-         range(2) for j in range(2)]
+    def blocks_draw(self):
+        for block in self.blocks:
+            block.draw(self.screen)
+
+    def blocks_collision_handler(self, index):
+        if index != -1:
+            block_rect = self.blocks.pop(index).rect
+            self.collision_detector(block_rect)
 
     def collision_detector(self, rect):
         if self.ball_x_direction < 0:
