c3b9d99d29fedfa2648ba70b2b435d7320b255db
AlexKubantsev <75995007+AlexKubantsev@users.noreply.github.com>
2021-01-13 12:11:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,23 +6,51 @@ import datetime as dt
 
 
 class Bonus:
-    FONT_SIZE = 60
-    BONUS_TEMPLATE = "YOU GIVE BONUS: {}"
-    FONT_SIZE_COEFF = 5
-    STANDART_COLOR = (255, 69, 0)
+    BONUS_SHOWING_TIME = 5
+    N_BLOCKS_FOR_GET_INCR_PLATFORM = 20
 
-    def __init__(self, window_width, window_height):
-        self.font = pygame.font.Font(None, self.FONT_SIZE)
-        self.win_width, self.win_height = window_width, window_height
+    def __init__(self, window_sizes, paddle, ball, blocks):
+        self.win_width, self.win_height = window_sizes
+        self.start_bonus_showing_time = None
+        self.paddle = paddle
+        self.ball = ball
+        self.n_blocks, self.m_blocks = GameWindow.N_BLOCKS, GameWindow.M_BLOCKS
+        self.blocks = blocks
 
-    def draw(self, surf, bonus_item):
-        bonus_string = self.BONUS_TEMPLATE.format(
-            bonus_item)
-        text = self.font.render(bonus_string, True, self.STANDART_COLOR)
-        surf.blit(text, (self.win_width//2 -
-                         len(bonus_string) *
-                         self.FONT_SIZE//self.FONT_SIZE_COEFF,
-                         self.win_height//2))
+    def is_bonus_can_get(self, n):
+        is_blocks_crashed = self.n_blocks * \
+            self.m_blocks - len(self.blocks) > 0
+        is_n_blocks_crashed = (
+            self.n_blocks * self.m_blocks - len(self.blocks)) % n == 0
+        return is_blocks_crashed and is_n_blocks_crashed
+
+    def try_get_incr_platform(self):
+        current_time = dt.datetime.now()
+        if self.start_bonus_showing_time is None \
+                and self.is_bonus_can_get(self.N_BLOCKS_FOR_GET_INCR_PLATFORM)\
+                and self.ball.get_lives() == Ball.STANDART_LIFES:
+            self.paddle.increase_width()
+            self.paddle.set_color(0, 0, 0)
+            self.ball.set_color(0, 0, 0)
+            self.paddle.gradient_effect()
+            self.ball.gradient_effect()
+            self.start_bonus_showing_time = current_time
+
+        elif self.start_bonus_showing_time is not None and \
+                (current_time -
+                 self.start_bonus_showing_time).seconds <= self.BONUS_SHOWING_TIME \
+                and self.ball.get_lives() == Ball.STANDART_LIFES:
+            self.paddle.gradient_effect()
+            self.ball.gradient_effect()
+        elif self.ball.get_lives() < Ball.STANDART_LIFES:
+            self.paddle.set_color(*Paddle.STANDART_COLOR)
+            self.ball.set_color(*Ball.STANDART_COLOR)
+            self.paddle.set_width(Paddle.WIDTH)
+            self.start_bonus_showing_time = None
+        else:
+            self.paddle.set_color(*Paddle.STANDART_COLOR)
+            self.ball.set_color(*Ball.STANDART_COLOR)
+            self.start_bonus_showing_time = None
 
 
 class Score:
@@ -33,10 +61,10 @@ class Score:
     FONT_SIZE_COEFF = 5
     STANDART_COLOR = (0, 255, 0)
 
-    def __init__(self, window_width, window_height):
+    def __init__(self, window_sizes):
         self.score = 0
         self.font = pygame.font.Font(None, self.FONT_SIZE)
-        self.win_width, self.win_height = window_width, window_height
+        self.win_width, self.win_height = window_sizes
 
     def up_score(self):
         self.score += 1
@@ -63,15 +91,44 @@ class Ball(pygame.sprite.Sprite):
     LIVES_TEMPLATE = "Lives left: {}"
     Y_INDENT_COEFF = 0.95
 
-    def __init__(self, game_window):
-        super().__init__(game_window.all_sprites)
-        self.win_width, self.win_height = game_window.get_window_size()
+    def __init__(self, window_sizes, all_sprites):
+        super().__init__(all_sprites)
+        self.win_width, self.win_height = window_sizes
         self.radius = self.RADIUS
         self.rect = pygame.Rect(0, 0, self.radius * 2, self.radius * 2)
         self.speed = self.SPEED
         self.color = self.STANDART_COLOR
         self.font = pygame.font.Font(None, self.FONT_SIZE)
         self.lives = self.STANDART_LIFES
+        self.is_ball_static = True
+        self.x_direction = self.y_direction = 1
+
+    def invert_x_direction(self):
+        self.x_direction = -self.x_direction
+
+    def invert_y_direction(self):
+        self.y_direction = -self.y_direction
+
+    def get_x_direction(self):
+        return self.x_direction
+
+    def get_y_direction(self):
+        return self.y_direction
+
+    def set_static(self, state):
+        self.is_ball_static = state
+
+    def centering(self, paddle_x, paddle_y, paddle_width, paddle_height):
+
+        centering_x = paddle_x + (paddle_width -
+                                  self.get_radius())//2
+        centering_y = paddle_y - paddle_height -\
+            self.get_radius()
+        self.set_pos(centering_x, centering_y)
+        self.set_static(True)
+
+    def static_state(self):
+        return self.is_ball_static
 
     def draw(self, surf):
         pygame.draw.ellipse(surf, self.color, self.rect)
@@ -105,9 +162,9 @@ class Ball(pygame.sprite.Sprite):
     def get_radius(self):
         return self.radius
 
-    def update(self, direction_x, direction_y):
-        self.rect.x += direction_x * self.speed
-        self.rect.y += direction_y * self.speed
+    def update(self):
+        self.rect.x += self.x_direction * self.speed
+        self.rect.y += self.y_direction * self.speed
 
 
 class Block(pygame.sprite.Sprite):
@@ -115,16 +172,21 @@ class Block(pygame.sprite.Sprite):
     HEIGHT = 32
     INDENT = 5
 
-    def __init__(self, game_window, x=0, y=0):
-        super().__init__(game_window.all_sprites)
+    def __init__(self, all_sprites, x=0, y=0):
+        super().__init__(all_sprites)
         self.width = self.WIDTH
         self.height = self.HEIGHT
         self.rect = pygame.Rect(x, y, self.width, self.height)
         self.image = GameWindow.load_image(
             "block" + str(randint(1, 6)) + ".png")
+        self.block_crashed_sound = pygame.mixer.Sound("data/block_crashed.mp3")
 
     def draw(self, surf):
         surf.blit(self.image, self.rect)
+
+    def play_crashed_effect(self, is_game_volumes_on):
+        if is_game_volumes_on:
+            self.block_crashed_sound.play()
 
 
 class Paddle(pygame.sprite.Sprite):
@@ -135,28 +197,44 @@ class Paddle(pygame.sprite.Sprite):
     BONUS_WIDTH_INCREASE = 75
     STANDART_COLOR = (192, 192, 192)
 
-    def __init__(self, game_window):
-        super().__init__(game_window.all_sprites)
-        self.win_width, self.win_height = game_window.get_window_size()
+    def __init__(self, window_sizes, all_sprites):
+        super().__init__(all_sprites)
+        self.win_width, self.win_height = window_sizes
         self.width, self.height = self.WIDTH, self.HEIGHT
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.speed = self.SPEED
-        self.win_width, self.win_height = game_window.get_window_size()
         self.color = self.STANDART_COLOR
+        self.paddle_touch_sound = pygame.mixer.Sound(
+            "data/paddle_touch_sound.wav")
+        self.direction = 1
 
-    def move_paddle(self, direction):
-        if direction < 0 and self.rect.x > 0:
-            self.rect.x += direction * self.speed
-        elif direction > 0 and self.rect.x < self.win_width - self.width:
-            self.rect.x += direction * self.speed
+    def play_touch_effect(self, is_game_volumes_on):
+        if is_game_volumes_on:
+            self.paddle_touch_sound.play()
+
+    def move(self):
+        if self.direction < 0 and self.rect.x > 0:
+            self.rect.x += self.direction * self.speed
+        elif self.direction > 0 and self.rect.x < self.win_width - self.width:
+            self.rect.x += self.direction * self.speed
+
+    def set_direction(self, val):
+        self.direction = val
+
+    def get_direction(self):
+        return self.direction
 
     def increase_width(self):
         self.width += self.BONUS_WIDTH_INCREASE
         self.rect.w = self.width
         x, y = self.get_pos()
-
         if x + self.width > self.win_width:
             self.set_pos(self.win_width - self.width, y)
+
+    def centering(self):
+        centering_paddle_x = (self.win_width - self.get_width())//2
+        centering_paddle_y = self.win_height * self.Y_INDENT_COEFF
+        self.set_pos(centering_paddle_x, centering_paddle_y)
 
     def gradient_effect(self):
         self.color = (randint(50, 255), randint(50, 255), randint(50, 255))
@@ -192,17 +270,12 @@ class GameWindow:
     COLLISION_EPSILON = 10
     LOSE = 0
     WIN = 1
-    BONUS_SHOWING_TIME = 5
-    N_BLOCKS_FOR_GET_INCR_PLATFORM = 20
 
     def __init__(self, width, height, screen):
         self.width = width
         self.height = height
         self.opened_menu = None
         self.screen = screen
-        self.block_crashed_sound = pygame.mixer.Sound("data/block_crashed.mp3")
-        self.paddle_touch_sound = pygame.mixer.Sound(
-            "data/paddle_touch_sound.wav")
         self.lose_sound = pygame.mixer.Sound("data/lose_sound.mp3")
         self.win_sound = pygame.mixer.Sound("data/win_sound.wav")
         self.game_background = self.load_image("game_background.jpg")
@@ -210,9 +283,9 @@ class GameWindow:
     def ui_initial(self):
         self.is_game_volumes_on = self.opened_menu.game_volumes_state()
         self.pause = False
-        self.score = Score(self.width, self.height)
-        self.bonus = Bonus(self.width, self.height)
-        self.start_bonus_showing_time = None
+        self.score = Score(self.get_window_size())
+        self.bonus = Bonus(self.get_window_size(),
+                           self.paddle, self.ball, self.blocks)
         self.is_key_downed = False
         self.clock = pygame.time.Clock()
         self.pause_menu = PauseMenu(
@@ -223,68 +296,31 @@ class GameWindow:
 
     def game_objects_initial(self):
         self.all_sprites = pygame.sprite.Group()
-        self.paddle = Paddle(self)
-        self.paddle.set_pos(
-            self.width//2, Paddle.Y_INDENT_COEFF * self.height)
-        self.is_ball_static = True
-        self.paddle_direction = 1
-        self.ball = Ball(self)
-        paddle_x, paddle_y = self.paddle.get_pos()
-        self.ball.set_pos(paddle_x + (self.paddle.get_width() -
-                                      self.ball.get_radius())//2,
-                          paddle_y - self.ball.radius * 2)
-        self.ball_x_direction = self.ball_y_direction = 1
+        self.paddle = Paddle(self.get_window_size(), self.all_sprites)
+        self.paddle.centering()
 
-    def is_bonus_can_get(self, n):
-        is_blocks_crashed = self.N_BLOCKS * \
-            self.M_BLOCKS - len(self.blocks) > 0
-        is_n_blocks_crashed = (
-            self.N_BLOCKS * self.M_BLOCKS - len(self.blocks)) % n == 0
-        return is_blocks_crashed and is_n_blocks_crashed
-
-    def try_get_incr_platform(self):
-        current_time = dt.datetime.now()
-        if self.start_bonus_showing_time is None \
-                and self.is_bonus_can_get(self.N_BLOCKS_FOR_GET_INCR_PLATFORM)\
-                and self.ball.get_lives() == Ball.STANDART_LIFES:
-            self.bonus.draw(self.screen, "INCREASING THE PLATFORM")
-            self.paddle.increase_width()
-            self.paddle.set_color(0, 0, 0)
-            self.ball.set_color(0, 0, 0)
-            self.paddle.gradient_effect()
-            self.ball.gradient_effect()
-            self.start_bonus_showing_time = current_time
-
-        elif self.start_bonus_showing_time is not None and \
-                (current_time -
-                 self.start_bonus_showing_time).seconds <= self.BONUS_SHOWING_TIME \
-                and self.ball.get_lives() == Ball.STANDART_LIFES:
-            self.paddle.gradient_effect()
-            self.ball.gradient_effect()
-            self.bonus.draw(self.screen, "INCREASING THE PLATFORM")
-        elif self.ball.get_lives() < Ball.STANDART_LIFES:
-            self.paddle.set_color(*Paddle.STANDART_COLOR)
-            self.ball.set_color(*Ball.STANDART_COLOR)
-            self.paddle.set_width(Paddle.WIDTH)
-        else:
-            self.paddle.set_color(*Paddle.STANDART_COLOR)
-            self.ball.set_color(*Ball.STANDART_COLOR)
+        self.ball = Ball(self.get_window_size(), self.all_sprites)
+        self.ball.centering(
+            *self.paddle.get_pos(),
+            self.paddle.get_width(), self.paddle.get_height())
 
     def bonus_get_handler(self):
-        self.try_get_incr_platform()
+        self.bonus.try_get_incr_platform()
 
     def start_game(self):
-        self.ui_initial()
         self.game_objects_initial()
-        self.running = True
         self.blocks_placement()
+        self.ui_initial()
+
+        self.running = True
+
         while self.running:
             self.events_handler()
             if not self.pause:
                 self.opened_menu.close_menu()
                 if self.is_key_downed:
-                    self.paddle.move_paddle(self.paddle_direction)
-                    self.is_ball_static = False
+                    self.paddle.move()
+                    self.ball.set_static(False)
                 self.update_game_loop()
                 self.win_lost_detector()
                 block_hit_index = self.ball.rect.collidelist(
@@ -295,18 +331,6 @@ class GameWindow:
             else:
                 self.pause_menu.draw()
 
-    def ball_rebirth(self):
-        new_paddle_x = (self.width - self.paddle.get_width())//2
-        new_paddle_y = self.paddle.get_pos()[1]
-        self.paddle.set_pos(new_paddle_x, new_paddle_y)
-        x_paddle, y_paddle = self.paddle.get_pos()
-        paddle_center_x = x_paddle + (self.paddle.get_width() -
-                                      self.ball.get_radius())//2
-        paddle_center_y = y_paddle - self.paddle.get_height() -\
-            self.ball.get_radius()
-        self.ball.set_pos(paddle_center_x, paddle_center_y)
-        self.is_ball_static = True
-
     def update_game_loop(self):
         self.is_game_volumes_on = self.opened_menu.game_volumes_state()
         self.screen.fill((0, 0, 0))
@@ -314,8 +338,8 @@ class GameWindow:
                          self.game_background.get_rect())
         self.score.draw(self.screen)
         self.ball.draw_lifes(self.screen)
-        if not self.is_ball_static:
-            self.ball.update(self.ball_x_direction, self.ball_y_direction)
+        if not self.ball.static_state():
+            self.ball.update()
         self.paddle.draw(self.screen)
         self.ball.draw(self.screen)
         self.blocks_draw()
@@ -324,20 +348,12 @@ class GameWindow:
         pygame.display.flip()
         self.clock.tick(GameWindow.FPS)
 
-    def play_block_crashed_effect(self):
-        if self.is_game_volumes_on:
-            self.block_crashed_sound.play()
-
     def play_game_end_effect(self, state):
         if self.is_game_volumes_on:
             if state == self.WIN:
                 self.win_sound.play()
             else:
                 self.lose_sound.play()
-
-    def play_paddle_touch_effect(self):
-        if self.is_game_volumes_on:
-            self.paddle_touch_sound.play()
 
     def get_window_size(self):
         return self.width, self.height
@@ -351,7 +367,6 @@ class GameWindow:
                  self.opened_menu.volume_control).draw()
         ball_y = self.ball.get_pos()[1]
         paddle_y = self.paddle.get_pos()[1]
-
         if ball_y > paddle_y + self.paddle.get_height():
             self.ball.decrease_lifes()
 
@@ -362,12 +377,16 @@ class GameWindow:
                      self, self.score.get_score(), self.LOSE,
                      self.opened_menu.volume_control).draw()
             else:
-                self.ball_rebirth()
+                self.paddle.set_width(Paddle.WIDTH)
+                self.paddle.centering()
+                self.ball.centering(
+                    *self.paddle.get_pos(), self.paddle.get_width(),
+                    self.paddle.get_height())
 
     def collision_handler(self):
         if self.ball.rect.colliderect(self.paddle.rect)\
-                and self.ball_y_direction > 0:
-            self.play_paddle_touch_effect()
+                and self.ball.get_y_direction() > 0:
+            self.paddle.play_touch_effect(self.is_game_volumes_on)
             self.collision_detector(self.paddle.rect)
 
         up_collision_line = self.ball.rect.clipline(0, 0, self.width, 0)
@@ -376,15 +395,15 @@ class GameWindow:
         left_collision_line = self.ball.rect.clipline(0, 0, 0, self.height)
 
         if right_collision_line:
-            self.ball_x_direction = -self.ball_x_direction
+            self.ball.invert_x_direction()
         if left_collision_line:
-            self.ball_x_direction = -self.ball_x_direction
+            self.ball.invert_x_direction()
         if up_collision_line:
-            self.ball_y_direction = -self.ball_y_direction
+            self.ball.invert_y_direction()
 
     def blocks_placement(self):
         self.blocks = [
-            Block(self,
+            Block(self.all_sprites,
                   i * (Block.WIDTH + Block.INDENT),
                   j * (Block.HEIGHT + Block.INDENT))
             for j in range(GameWindow.N_BLOCKS)
@@ -400,27 +419,27 @@ class GameWindow:
 
     def blocks_collision_handler(self, index):
         if index != -1:
-            self.play_block_crashed_effect()
             self.score.up_score()
-            block_rect = self.blocks.pop(index).rect
-            self.collision_detector(block_rect)
+            block = self.blocks.pop(index)
+            block.play_crashed_effect(self.is_game_volumes_on)
+            self.collision_detector(block.rect)
 
     def collision_detector(self, rect):
-        if self.ball_x_direction > 0:
+        if self.ball.get_x_direction() > 0:
             dx = self.ball.rect.right - rect.left
         else:
             dx = rect.right - self.ball.rect.left
-        if self.ball_y_direction > 0:
+        if self.ball.get_y_direction() > 0:
             dy = self.ball.rect.bottom - rect.top
         else:
             dy = rect.bottom - self.ball.rect.top
         if abs(dx - dy) < self.COLLISION_EPSILON:
-            self.ball_x_direction, self.ball_y_direction = - \
-                self.ball_x_direction, -self.ball_y_direction
+            self.ball.invert_x_direction()
+            self.ball.invert_y_direction()
         elif dx > dy:
-            self.ball_y_direction = -self.ball_y_direction
+            self.ball.invert_y_direction()
         elif dx < dy:
-            self.ball_x_direction = -self.ball_x_direction
+            self.ball.invert_x_direction()
 
     def events_handler(self):
         for event in pygame.event.get():
@@ -430,12 +449,12 @@ class GameWindow:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     self.is_key_downed = True
-                    self.paddle_direction = 1
-                    self.is_ball_static = False
+                    self.paddle.set_direction(1)
+                    self.ball.set_static(False)
                 elif event.key == pygame.K_LEFT:
                     self.is_key_downed = True
-                    self.paddle_direction = -1
-                    self.is_ball_static = False
+                    self.paddle.set_direction(-1)
+                    self.ball.set_static(False)
                 elif event.key == pygame.K_ESCAPE:
                     self.pause_handler()
             if event.type == pygame.KEYUP:
